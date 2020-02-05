e8ec7f0e024d7db54936f016d1e976ce7cf15619
TANG ZHIXIONG <zhixiong@momenta.ai>
2020-02-05 10:17:18 +0800

    fold flashcards

--- generate_flashcards.py
+++ generate_flashcards.py
@@ -22,12 +22,10 @@ if __name__ == '__main__':
     with open(path) as f:
         lines = f.readlines()
     cards = []
-    index = 0
     for line in lines:
         line = line.strip()
         if not line:
             continue
-        index += 1
         pinyin = [k[0] for k in pypinyin.pinyin(line, style=pypinyin.Style.NORMAL, errors='ignore')]
         cache = {}
         trans = {}
@@ -39,24 +37,29 @@ if __name__ == '__main__':
                 translated=trans,
             ) for py in pinyin
         ]
-        cards.append(f'{output_directory}/card_{index:08d}.md')
+        py2sp = [[py, sp] for py, sp in trans.items() if py != sp]
+        if not py2sp:
+            continue
+        cards.append(f'{output_directory}/card_{len(cards):08d}.md')
         with open(cards[-1], 'w') as f:
-            # f.write('<br><br><br><br>\n\n')
-            py2sp = [[py, sp] for py, sp in trans.items() if py != sp]
-            if not py2sp:
-                continue
+            if len(line) < 40:
+                prefix = f'        '
+                line = line.replace('\n', ';')
+                f.write(f'-   "{line}" -<\n\n    :   ')
+            else:
+                prefix = ''
+                f.write(f'{line}\n')
             if py2sp:
-                f.write('| 拼音 | 双拼 |\n')
-                f.write('| :--- | :--: |\n')
+                f.write(f'| 拼音 | 双拼 |\n')
+                f.write(f'{prefix}| :--- | :--: |\n')
                 for (py, sp) in py2sp:
                     if py == sp:
                         continue
-                    f.write(f'| {py} | {sp} |\n')
-            f.write('\n```\n')
-            f.write(''.join(shuangpin))
-            f.write('\n')
-            f.write(line)
-            f.write('\n```\n')
+                    f.write(f'{prefix}| {py} | {sp} |\n')
+            f.write(f'\n{prefix}```')
+            f.write(f'\n{prefix}{"".join(shuangpin)}')
+            f.write(f'\n{prefix}{line}')
+            f.write(f'\n{prefix}```\n')
 
     with open(f'{output_directory}/index.md', 'w') as f:
         f.write('# Cards\n')
